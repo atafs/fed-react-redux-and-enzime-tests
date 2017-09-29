db8c06020915118ea0b5d4f1676013453f95da79
Americo Tomas <atomas@totallymoney.com>
2017-09-30 00:40:12 +0100

    section3: 12 Rendering JSX onto the DOM

--- es6/classes.js
+++ es6/classes.js
@@ -9,7 +9,20 @@ class Animal {
     }
 }
 
-const lion = new Animal('Mufasa', 'golder')
+class Lion extends Animal {
+    constructor(name, color, role, home) {
+        super(name, color);
+        this.role = role;
+        this.home = home;
+    }
+
+    rore() {
+        console.log(`I'm the ${this.role} of ${this.home}`);
+    }
+}
+
+const lion = new Lion('Mufasa', 'golder', 'king', 'Pride rock')
 console.log(lion);
 
 lion.speak();
+lion.rore();
