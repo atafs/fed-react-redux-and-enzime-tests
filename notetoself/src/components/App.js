36b75cde2555714bccadbe6f021fdfc1789f688f
Americo Tomas <atomas@totallymoney.com>
2017-10-03 21:44:16 +0100

    section3: 22 => app NoteToSelf

--- notetoself/src/components/App.js
+++ notetoself/src/components/App.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+
+import Note from './Note';
 
 import '../style/app.css';
+
+//global props
+const cookie_key = 'NOTES';
 
 class App extends Component {
     constructor(){
         super();
 
         this.state = {
-            text: ''
+            text: '',
+            notes: []
         }
     }
+
+    componentDidMount() {
+        this.setState( { notes: read_cookie(cookie_key) } );
+    }
+
+    submit() {
+        const { notes, text } = this.state;
+        notes.push( { text } );
+        this.setState({ notes });
+
+        bake_cookie(cookie_key, this.state.notes);
+    }
+
+    clear() {
+        delete_cookie(cookie_key);
+        this.setState({ notes: [] });
+    }
+
     render() {
         return (
             <div>
                 <h2>Note to Self</h2>
                 <Form inline>
                     <FormControl className='form-control'  onChange={ event => this.setState({ text: event.target.value}) }/>
-                    <Button onClick={() => console.log(this.state)} >Submit</Button>
+                    <Button onClick={() => this.submit()} >Submit</Button>
                 </Form>
+                {
+                    this.state.notes.map( (note, index) => {
+                        return (
+                            <Note key={index} note={note} />
+                        )
+                    })
+                }
+                <hr />
+                <Button onClick={ () => this.clear() }>Clear Notes</Button>
             </div>
             
         )
     }
 }
-
-export const color = 'blue';
-export const number = 22;
 
 export default App;
--- notetoself/src/index.js
+++ notetoself/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App, { color, number } from './components/App';
-
-console.log('export variables color and number:', color, number);
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
