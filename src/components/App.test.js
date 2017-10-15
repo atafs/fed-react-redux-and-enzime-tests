74911c62b1997a6497aa6a399cb46492c53cf6a2
Americo Tomas <atomas@totallymoney.com>
2017-10-15 15:26:38 +0100

    section4: 31 => reporting test coverage

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -28,4 +28,57 @@ describe('App', () => {
             expect(app.find('.btn').at(0).text()).toEqual('Submit');
         });
     });
+
+    describe('when creating a note', () => {
+        let testNote = 'test note';
+
+        beforeEach(() => {
+            app.find('FormControl').simulate('change', {
+                target: { value: testNote }
+            });
+        });
+
+        it('updates the text in state', () => {
+            expect(app.state().text).toEqual(testNote);
+        });
+
+        describe('and submitting the new note', () => {
+            beforeEach(() => {
+                app.find('.btn').at(0).simulate('click');
+            });
+
+            afterEach(() => {
+                app.find('.btn').at(1).simulate('click');
+            });
+
+            it('adds the new to state', () => {
+                console.log(app.state());
+                expect(app.state().notes[0].text).toEqual(testNote);
+            });
+
+            describe('and remounting the component', () => {
+                let app2;
+
+                beforeEach(() => {
+                    app2 = mount(<App />);
+                });
+
+                it('reads the stored note cookies', () => {
+                    console.log(app2.state());
+                    expect(app2.state().notes).toEqual([{text: testNote}]);
+                });
+            });
+
+            describe('and clicking the clear button', () => {
+                beforeEach(() => {
+                    app.find('.btn').at(1).simulate('click');
+                });
+
+                it('clears the notes in state', () => {
+                    console.log(app.state());
+                    expect(app.state().notes).toEqual([]);
+                });
+            });
+        });
+    });
 });
